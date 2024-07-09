2cf90d6650b46eec279a61a022fc233a6ed9af7f
Dhruvikankadiya1337 <dhruvikankadiya@gmail.com>
2024-07-09 10:00:33 +0530

    Add files via upload

---
+++ printf q 2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	printf("name :kankapariya dhruvi rameshbhsi\n");
+	printf("Age:-18\n");
+	printf("school name:- leo school of science& commerce");
+}
